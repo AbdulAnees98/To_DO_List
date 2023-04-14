402f006d2d1d65233f91b6490819692cd2376a71
Abdulaneesabdulsalim Quereshi <abdulaneesabdulsalim.quereshi@genzeon.com>
2023-04-14 14:47:38 +0530

    project is build

--- src/Redux/Reducer/TodoListSlice.js
+++ src/Redux/Reducer/TodoListSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  tasklist: [],
+  tasklist: ["Electric bill","compelete Homework","send money"],
 };
 const TodoSlice = createSlice({
   name: "Todo",
